faa0d47355e3c91514cde6b14426fda74d5e896d
sstremler <stremler.sz@gmail.com>
2020-02-16 23:54:44 +0100

    first commit

---
+++ stock-price-generator/src/main/java/io/stremler/stockpricegenerator/StockPriceGeneratorApplication.java
@@ -0,0 +1,13 @@
+package io.stremler.stockpricegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPriceGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPriceGeneratorApplication.class, args);
+	}
+
+}
---
+++ stock-price-generator/src/test/java/io/stremler/stockpricegenerator/StockPriceGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.stremler.stockpricegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPriceGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
