44a3d0d4875a044153c76c06f09bc1edef1edf67
sstremler <stremler.sz@gmail.com>
2020-03-02 21:03:07 +0100

    add service module

---
+++ stock-price-service/src/main/java/io/stremler/stockpriceservice/StockPriceServiceApplication.java
@@ -0,0 +1,13 @@
+package io.stremler.stockpriceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPriceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPriceServiceApplication.class, args);
+	}
+
+}
